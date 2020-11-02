117577945c3c49ee6fd59f2e70e377dbeeb52c2b
senior24 <vitalij.baev.98@bk.ru>
2020-11-02 12:02:07 +0300

    добавил изменение в gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const cleanCss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify-es').default;
 const imagemin = require("gulp-imagemin");
-const webp = require('gulp-webp');
+// const webp = require('gulp-webp');
 // const webpHtml = require('gulp-webp-html');
 // const ttf2woff = require('gulp-ttf2woff');
 // const ttf2woff2 = require('gulp-ttf2woff2');
@@ -30,14 +30,14 @@ const path = {
       css: sourceFolder + "/assets/scss/style.scss",
       js: sourceFolder + "/assets/js/index.js",
       img: sourceFolder + "/assets/img/**/*.{jpg,png,svg,gif,ico,webp}",
-      fonts: sourceFolder + "/assets/fonts/**/*.{ttf, eot, woff, woff2}",
+      fonts: sourceFolder + "/assets/fonts/**/*.{ttf,eot,woff,woff2}",
    },
    watch: {
       html: sourceFolder +"/**/*.html",
       css: sourceFolder + "/assets/scss/**/*.scss",
       js: sourceFolder + "/assets/js/**/*.js",
       img: sourceFolder + "/assets/img/**/*.{jpg,png,svg,gif,ico,webp}",
-      fonts: sourceFolder + "/assets/fonts/**/*.{ttf, eot, woff, woff2}",
+      fonts: sourceFolder + "/assets/fonts/**/*.{ttf,eot,woff,woff2}",
    },
    clean: "./" + projectFolder + "/"
 };
@@ -98,11 +98,11 @@ function js() {
  
 function images() {
    return src(path.src.img)
-      .pipe(
-         webp({
-            quality: 70
-         })
-      )
+      // .pipe(
+      //    webp({
+      //       quality: 70
+      //    })
+      // )
       .pipe(dest(path.build.img))
       .pipe(src(path.src.img))
       .pipe(
